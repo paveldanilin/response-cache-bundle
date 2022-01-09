332713d0660ad079dd26a2b5fa4e364dd9f667ca
Pavel <pavel.k.danilin@gmail.com>
2022-01-10 00:29:09 +1000

    Fix

--- src/DependencyInjection/ResponseCacheExtension.php
+++ src/DependencyInjection/ResponseCacheExtension.php
@@ -2,15 +2,19 @@
 
 namespace Pada\ResponseCacheBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader;
 
 class ResponseCacheExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $config = $this->processConfiguration(new Configuration(), $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
 
         $cacheableDefinition = $container->getDefinition('cacheable_service');
         $cacheableDefinition->replaceArgument(5, $config['lock']['store'] ?? null);
