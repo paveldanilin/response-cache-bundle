d2baf88ef73b96d75228427f7f39beae238f9b42
Pavel <pavel.k.danilin@gmail.com>
2022-01-10 01:09:53 +1000

    Fix

--- src/DependencyInjection/ResponseCacheExtension.php
+++ src/DependencyInjection/ResponseCacheExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Reference;
 
 class ResponseCacheExtension extends Extension
 {
@@ -16,7 +17,11 @@ class ResponseCacheExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
 
+        $cacheWarmer = $container->getDefinition('response_cache_bundle_cache_warmer');
+        $cacheWarmer->replaceArgument(0, $config['controller']['dir']);
+
         $cacheableDefinition = $container->getDefinition('cacheable_service');
         $cacheableDefinition->replaceArgument(6, $config['lock']['store'] ?? 'flock');
+        $cacheableDefinition->addMethodCall('setLogger', [new Reference('logger')]);
     }
 }
